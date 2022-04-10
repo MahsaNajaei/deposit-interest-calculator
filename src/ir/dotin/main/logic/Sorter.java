028e186a343caa53d2ad1b2230de8dc4983a523f
Mahsa Najaei <m.najaei@gmail.com>
2022-04-10 09:54:51 +0430

    lambda expression is replaced with a part of code in sorter file

--- src/ir/dotin/main/logic/Sorter.java
+++ src/ir/dotin/main/logic/Sorter.java
@@ -6,12 +6,7 @@ import java.util.TreeMap;
 
 public interface Sorter<K, V> {
     static <K, V extends Comparable<V>> Map<K, V> sortMapByValue(Map<K, V> map) {
-        Map sortedMap = new TreeMap(new Comparator<K>() {
-            @Override
-            public int compare(K key1, K key2) {
-                return map.get(key2).compareTo(map.get(key1));
-            }
-        });
+        Map sortedMap = new TreeMap((Comparator<K>) (key1, key2) -> map.get(key2).compareTo(map.get(key1)));
 
         sortedMap.putAll(map);
 
